4d0a2d663cc141049e21f5fbe471568c13dbca2f
Yoonho Park <pyh051920@gmail.com>
2025-11-22 16:13:36 -0600

    edit : firebase key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,14 +5,13 @@ import { getStorage } from 'firebase/storage'
 import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-}
+  apiKey: "AIzaSyA58kkRLwf4Sq3i6nv4ACbuO5x5ZgvSWY0",
+  authDomain: "choonsik-madhack.firebaseapp.com",
+  projectId: "choonsik-madhack",
+  storageBucket: "choonsik-madhack.firebasestorage.app",
+  messagingSenderId: "2342628402",
+  appId: "1:2342628402:web:3c26e95d6503c72e7d202b"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
